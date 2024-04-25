d8a64b9f674cd5c9c142138b2bb2af0f2bbdd195
koushikprasath <koushikvprasath@gmail.com>
2024-04-25 13:50:24 +0530

    first commit

---
+++ css,js/add.js
@@ -0,0 +1,146 @@
+//EXERCISE:2
+//Addition in function
+// var a=34
+// var b=66
+// function total()
+// {
+//     console.log(a+b)
+// }
+// total()
+
+//EXERCIES:3
+//Merge the words its also without using functions
+// var actor="Kamal"
+// var fplayer="Dhoni"
+// var fmovie="Infinite war"
+// function Favourite()
+// {
+// console.log("Favourite Actor:"+actor)
+// console.log("Favourite Player:"+fplayer)
+// console.log("Favourite Movie:"+fmovie)
+// }
+// Favourite()
+// Favourite()
+//call many times at time
+
+//EXERCISE:4
+
+// function add(a,b)
+// {
+//     return (a+b)
+// }
+// var total=add(10,20)
+// console.log(total)
+
+//EXERCISE:5
+//IF ELSE Condition
+
+// var rain=false
+// if(rain)
+//  {
+//     console.log("take an umbrella")
+//  }
+//  else
+//  {
+//     console.log("enjoy the sunshine")
+//  }
+ 
+//EXERCISE:6
+// var color="yellow"
+// if(color == "red") 
+// {
+//     console.log("stop")
+// }
+// if(color == "yellow")
+// {
+//     console.log("ready to go")
+// }
+// if(color == "green")
+// {
+//     console.log("Go")
+// }
+
+//EXERCISE:7
+
+// var score=70
+// if(score<=50)
+// {
+//     console.log("you need to improve")
+// }
+// if(score>50 && score<=70)
+// {
+//     console.log("Good job")
+// }
+// if(score>70)
+// {
+//     console.log("Very Good")
+// }
+//EXERCIES:7.1
+// function isVowel(char) {
+//     char = char.toLowerCase();
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u')
+// {
+//     console.log("It's a Vowel")
+//     return true;
+// }
+// else {
+//     return false;
+// }
+// }
+//EXERCIES:7.2
+// function checkOddEven(number) {
+//     if (number % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// }
+// console.log(checkOddEven(13)); 
+// console.log(checkOddEven(7));
+//EXERCIES:8.1
+//For Loop string
+
+// for (count=1;count<=5;count=count+1)
+// {
+//     console.log("koushik")
+// }
+
+//EXERCISE:8.2
+//number for loop
+
+// for (count=1;count<=10;count=count+1)
+//  {
+//      console.log(count)
+//  }
+
+//EXERCISE:8.3
+
+// for(count=1;count<=10;count=count+2)
+// {
+//     console.log(count)
+// }
+//output:13579
+
+//EXERCISE:8.4
+
+// for(count=10;count>=1;count=count-1)
+// {
+//     console.log(count)
+// }
+//OUTPUT:10987654321
+
+//EXERCISE:8.5
+
+// for(count=2;count<=10;count+=2)
+// {
+//     console.log(count)
+// }
+//even number
+
+//EXERCISE:8.6
+
+// for (let i = 1; i <= 10; i++)
+// {
+//   console.log("2 * " + i + " = " + (2 * i));
+// }
+//OUTPUT:2 tables 
---
+++ css,js/function.js
@@ -0,0 +1,20 @@
+function samsung()
+{
+    console.log("This is samsung")
+}
+samsung()
+function iphone()
+{
+    console.log("This is iphone")
+}
+iphone()
+function oppo()
+{
+    console.log("This is oppo")
+}
+oppo()
+function redmi()
+{
+    console.log("This is redmi")
+}
+redmi()
